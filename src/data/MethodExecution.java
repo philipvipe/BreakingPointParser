5ff11c3e4d8de1000cba817162f561e517771202
Austin Bourgerie <bourgerie.a@gmail.com>
2018-05-16 00:09:57 -0400

    New method of tracking branch usage

--- src/data/MethodExecution.java
+++ src/data/MethodExecution.java
@@ -2,17 +2,19 @@ package data;
 
 import java.util.Arrays;
 
+import static data.MethodExecution.TakenStatus.*;
+
 public class MethodExecution {
     private final String qualifiedName;
-    private int[] branches;
+    private TakenStatus[] branches;
 
-    private MethodExecution(String qualifiedName, int[] branches) {
+    private MethodExecution(String qualifiedName, TakenStatus[] branches) {
         this.qualifiedName = qualifiedName;
         this.branches = branches;
     }
 
     public MethodExecution(String qualifiedName) {
-        this(qualifiedName, new int[0]);
+        this(qualifiedName, new TakenStatus[0]);
     }
 
     public boolean isSameMethod(MethodExecution other) {
@@ -38,25 +40,35 @@ public class MethodExecution {
             withLess = this;
         }
 
-        int[] newBranches = Arrays.copyOf(withMore.branches, withMore.branches.length);
+        TakenStatus[] newBranches = Arrays.copyOf(withMore.branches, withMore.branches.length);
 
         for (int i = 0; i < withLess.branches.length; i++) {
-            newBranches[i] += withLess.branches[i];
+            if (newBranches[i] == UNSEEN) {
+                newBranches[i] = withLess.branches[i];
+            } else if ((newBranches[i] == NOT_TAKEN && withLess.branches[i] == TAKEN)
+                    || (newBranches[i] == TAKEN && withLess.branches[i] == NOT_TAKEN)
+                    || withLess.branches[i] == BOTH) {
+                newBranches[i] = BOTH;
+            }
         }
 
         return new MethodExecution(this.qualifiedName, newBranches);
     }
 
     public MethodExecution withBranch(int branchNum, boolean taken) {
-        int[] thisBranch = new int[branchNum + 1];
+        TakenStatus[] thisBranch = new TakenStatus[branchNum + 1];
 
-        thisBranch[branchNum] = taken ? 1 : 0;
+        thisBranch[branchNum] = taken ? TAKEN : NOT_TAKEN;
 
         return this.join(new MethodExecution(this.qualifiedName, thisBranch));
     }
 
-    public boolean wasBranchTaken(int branchNum) {
-        return timesBranchTaken(branchNum) > 0;
+    public TakenStatus branchStatus(int branchNum) {
+        if (branchNum >= branches.length) {
+            return UNSEEN;
+        }
+
+        return branches[branchNum];
     }
 
     public String getName() {
@@ -67,11 +79,7 @@ public class MethodExecution {
         return this.branches.length;
     }
 
-    public int timesBranchTaken(int branchNum) {
-        if (branchNum >= this.branches.length) {
-            return 0;
-        }
-
-        return this.branches[branchNum];
+    public enum TakenStatus {
+        UNSEEN, NOT_TAKEN, TAKEN, BOTH;
     }
 }
