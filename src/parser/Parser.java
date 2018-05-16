0c514a18def40d5350161781a9ffd2fe36192b51
Philip Ipe <philipvipe@gmail.com>
2018-05-15 20:53:39 -0400

    Changes to parser to use IExecutionTracer

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,20 +1,24 @@
 package parser;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Stack;
+
+import data.IExecutionTracer;
 
 public class Parser {
 
-	private Stack<String> methods = new Stack<String>(); // all the methods visited
-	private HashMap<String, Stack<String>> methodToMethods = new HashMap<String, Stack<String>>(); // maps a method to methods that called it
-	private Stack<String> methodsAndConditionals = new Stack<String>();
-
-	public static void main(String[] args) {
-		String filename = args[0];	// filename of breaking point output we want to parse
-		Parser parser = new Parser();
-		parser.readFile(filename);
+	private int checkForBranch = -1; // contains branch num if we are not sure if it branched or not
+	private IExecutionTracer tracer;
+	
+	public Parser(IExecutionTracer tracer){
+		this.tracer = tracer;
 	}
+	
+//	public static void main(String[] args) {
+//		//String filename = args[0];	// filename of breaking point output we want to parse
+//		Parser parser = new Parser();
+//		//parser.readFile(filename);
+//		parser.readFile("foo");
+//	}
 
 	public void readFile(String filename){
 		System.out.println("Parsing through " + filename);
@@ -30,10 +34,6 @@ public class Parser {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		for(String m: methodsAndConditionals){
-			System.out.println(m);
-		}
 	}
 
 	public void parseLine(String full_line){
@@ -43,52 +43,37 @@ public class Parser {
 		String line = full_line.substring(full_line.indexOf(":") + 2);
 		String arrow = line.substring(0,3);
 
+		// last line was a branch condition
+		if(checkForBranch != -1){
+			if(line.contains("Did not branch!")){
+				tracer.onBranch(checkForBranch, false);
+			}
+			else{
+				tracer.onBranch(checkForBranch, true);
+			}
+			checkForBranch = -1; // revert back to initial flag
+		}
+		
 		// conditional
 		if(arrow.contains("<") && arrow.contains(">")){
-
+			
 			line = line.substring(4);
-			methodsAndConditionals.add(line);
+			int index = line.indexOf("?");
+			checkForBranch = Integer.parseInt(line.substring(index - 1, index)); // tell parser to check next line to see if it branches			
 		}
 		// a method is calling another method
 		else if(arrow.contains(">")){
 
 			line = line.substring(4);
 			int space_index = line.indexOf(" ");
-			String callerMethod = line.substring(0, space_index);
 			String calleeMethod = line.substring(space_index + 1);
 			
-			Stack<String> q1;
-			if(methodToMethods.containsKey(calleeMethod)){
-				q1 = methodToMethods.get(calleeMethod);
-			}
-			else{
-				q1 = new Stack<String>();
-			}
-			q1.push(callerMethod);
-			methodToMethods.put(calleeMethod, q1);
-			if(methods.isEmpty()){
-				methods.add(callerMethod);
-				methodsAndConditionals.add(callerMethod);
-			}
-			if(!methods.peek().equals(calleeMethod)){
-				methods.add(calleeMethod);
-				methodsAndConditionals.add(calleeMethod);
-			}
+			tracer.onCall(calleeMethod);
 		}
 		else if(arrow.contains("<")){
-			line = line.substring(4);
-			
-			if(methodToMethods.containsKey(line)){
-				String callerMethod = methodToMethods.get(line).pop();
-				if(!methods.peek().equals(callerMethod)){
-					methods.add(callerMethod);
-					methodsAndConditionals.add(callerMethod);
-				}
-			}
-		}
-		else if(line.contains("Did not branch!")){
-			methodsAndConditionals.pop();
+			tracer.onReturn();
 		}
 	}
+
 
 }
