dd91b039bda90921393134ac81b2765d5b8fccfc
johannkm <jkmiller@umd.edu>
2018-05-15 21:23:05 -0400

    add qualified name to execution trace

--- src/data/ExecutionTrace.java
+++ src/data/ExecutionTrace.java
@@ -3,12 +3,18 @@ package data;
 import java.util.Set;
 
 public class ExecutionTrace {
+    private final String qualifiedName;
     private final boolean passing;
     private final Set<MethodExecution> executions;
 
-    public ExecutionTrace(boolean passing, Set<MethodExecution> executions) {
+    public ExecutionTrace(String qualifiedName, boolean passing, Set<MethodExecution> executions) {
+        this.qualifiedName = qualifiedName;
         this.passing = passing;
         this.executions = executions;
+    }
+
+    public String getName() {
+        return this.qualifiedName;
     }
 
     public boolean isPassing() {
